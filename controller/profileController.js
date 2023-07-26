943ee1425f963749bcf53271961f82bba3db6dbc
mohamedeld <mohamed.azoz20010@gmail.com>
2023-07-26 20:33:54 +0300

    add experience and delete

--- controller/profileController.js
+++ controller/profileController.js
@@ -1,4 +1,4 @@
-const Profile = require('../models/profileSchema');
+const Profile = require('../models/profileModel');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
@@ -52,25 +52,50 @@ exports.updateProfile = catchAsync(async (request,response,next)=>{
     profile.experience = request.body.experience;
     profile.education = request.body.education;
     profile.social = request.body.social;
-
-  // Save the updated profile to the database
   const updatedProfile = await profile.save();
   response.status(200).json({
     status: 'success',
     data: { profile: updatedProfile }
   });
-    // if(!request.body.user) request.body.user = request.user._id;
-    // const profile = await Profile.findByIdAndUpdate(request.params.id,request.body,{new:true });
-    // if(!profile){
-    //     return next(new AppError('there is no profile for this user',401));
-    // }
-    // response.status(200).json({
-    //     status:'success',
-    //     data:{
-    //         profile
-    //     }
-    // })
+    
 });
+
+exports.updateExperience = catchAsync(async (request,response,next)=>{
+    const profile = await Profile.findOne({user:request.user.id});
+   if(!profile){
+       return next(new AppError('there is no profile for this user',401));
+   }
+   const {title,company,location,from,to,current,description} = request.body;
+   const newExp = {title,company,location,from,to,current,description};
+   
+   profile.experience.unshift(newExp);
+   await profile.save();
+   console.log(profile.experience)
+   response.status(200).json({
+    status:'success',
+    message:'exdperience updated successfully',
+    data:{
+        profile
+    }
+   })
+});
+
+exports.deleteExperience = catchAsync(async(request,response,next)=>{
+    const profile = await Profile.findOne({user:request.user.id});
+    if(!profile){
+        return next(new AppError('there is no profile for this user',401));
+    }
+    const removeIndex = profile.experience.map((item)=>item.id).indexOf(request.params.exp_id);
+    profile.experience.splice(removeIndex,1);
+    await profile.save();
+    response.status(200).json({
+        status:'success',
+        message:'deleted successfully',
+        data:{
+            profile
+        }
+    })
+})
 
 exports.deleteProfile = catchAsync(async (request,response,next)=>{
     const profile = await Profile.findOne({user:request.user.id});
